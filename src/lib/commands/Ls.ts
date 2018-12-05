5b20855e73a7221aeeeed54711757ea58de54355
Alex Marchant <alexjmarchant@gmail.com>
2018-12-05 12:05:34 -0500

    Fix naming

--- src/lib/commands/Ls.ts
+++ src/lib/commands/Ls.ts
@@ -2,7 +2,7 @@ import Command from '../Command'
 import Terminal from '../Terminal'
 import * as FS from '../FS'
 
-export default class LS extends Command {
+export default class Ls extends Command {
   name = 'ls'
 
   async execute (terminal: Terminal): Promise<void> {
